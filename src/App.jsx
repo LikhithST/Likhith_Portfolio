150ac8842b45db0d217aef17291f3ebe41d983b9
LikhithST <likhithsadahallithammegowda@gmail.com>
2025-12-25 14:42:56 +0100

    changing from browser router to Hash router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -8,7 +8,7 @@ import AppRoutes from './routes';
 // 4. Main Layout
 function App() {
   return (
-    <BrowserRouter basename="/Likhith_Portfolio">
+    <HashRouter>
       <div className="layout">
         <Sidebar />
         <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
@@ -17,7 +17,7 @@ function App() {
           {/* <Footer /> */}
         </div>
       </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
